5e2b4131338aa6c050a8aa1e0d05b3da627dec51
Vigl3228 <enevigl2001@mail.ru>
2020-11-21 03:55:01 +0700

    zxc3

---
+++ main.cpp
@@ -0,0 +1,37 @@
+﻿
+#include <iostream>
+using namespace std;
+
+double my_pow(double x, int y) {
+    double q = 1;
+
+    if (y < 0) {
+        y = -y;
+        for (double i = 0; i < y; i++) {
+            q = q * x;
+        }
+
+        return 1 / q;
+    }
+    else {
+        for (double i = 0; i < y; i++) {
+            q = q * x;
+        }
+        cout << q << endl;
+        return q;
+    }
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    int a;
+    double b, q;
+    cout << "Введите степень, затем число" << endl;
+    cin >> a;
+    cin >> b;
+    q = my_pow(b, a);
+    cout << b << " в степени " << a << " = " << q;
+    return 0;
+}
+
+
